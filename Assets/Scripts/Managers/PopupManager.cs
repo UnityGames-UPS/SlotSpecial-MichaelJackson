2bd497fca63d3e7cda4348b836cea8675000aae2
PalaashDwivediUnderpin <palaash.dwivedi@underpinservices.org>
2026-09-18 17:26:15 +0530

    Popup text in script fix

--- Assets/Scripts/Managers/PopupManager.cs
+++ Assets/Scripts/Managers/PopupManager.cs
@@ -98,7 +98,7 @@ public class PopupManager : MonoBehaviour
 
     internal void ShowDisconnectionPopup()
     {
-        ShowDisconnectionPopup("Game disconnected due to network error. Please relaunch the game.");
+        ShowDisconnectionPopup("Please restart the game.");
     }
 
     internal void ShowDisconnectionPopup(string message)
